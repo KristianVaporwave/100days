e1a74cc92d0cdefee6f7e12a053c3bdb6fa127ac
Kristian David Stanković <kdstankovic@gmail.com>
2022-09-15 23:46:25 +0200

    Nesting and dictionaries

---
+++ Day 8/Nesting lists and dictionaries.py
@@ -0,0 +1,27 @@
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin",
+}
+
+cities_visited = {
+  "Paris": "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture.",
+  "Lille": "Lille is the capital of the Hauts-de-France region in northern France, near the border with Belgium.",
+  "Dijon": "Dijon is the capital city of the historical Burgundy region in eastern France, one of the country’s principal wine-making areas.",
+}
+
+travel_log = {
+  "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+  "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+travels_log = {
+  {"country": "France",
+   "cities_visited": ["Paris", "Lille", "Dijon"],
+   "total_visits": 12
+  },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 7
+  },
+}
