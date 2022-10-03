f77c76dbd975c6882affcf938e66165b69e181ed
Kristian David Stanković <47901046+KristianVaporwave@users.noreply.github.com>
2022-10-03 19:14:00 +0200

    Create Dumb way.py

---
+++ Day 22/Dumb way.py
@@ -0,0 +1,43 @@
+from turtle import *
+
+tim = Turtle()
+
+for _ in range(3):
+    tim.forward(100)
+    tim.right(120)
+    tim.forward(100)
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+
+for _ in range(5):
+    tim.forward(100)
+    tim.right(72)
+    tim.forward(100)
+
+for _ in range(6):
+    tim.forward(100)
+    tim.right(60)
+    tim.forward(100)
+
+for _ in range(7):
+    tim.forward(100)
+    tim.right(51.429)
+    tim.forward(100)
+
+for _ in range(8):
+    tim.forward(100)
+    tim.right(45)
+    tim.forward(100)
+
+for _ in range(9):
+    tim.forward(100)
+    tim.right(40)
+    tim.forward(100)
+
+for _ in range(10):
+    tim.forward(100)
+    tim.right(36)
+    tim.forward(100)
