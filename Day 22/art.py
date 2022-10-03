f1ac6a732409508a5358e31b1100692ab09a7b5c
Kristian David Stanković <47901046+KristianVaporwave@users.noreply.github.com>
2022-10-03 19:13:51 +0200

    Create art.py

---
+++ Day 22/art.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    b = random.randint(0, 255)
+    g = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+directions = [90, 360, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+
+
+for i in range(900):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
